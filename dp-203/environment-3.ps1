75384d7c3f0bf1c7895907951244be4ddb400a47
Matt Boothman <matt.boothman@godeploy.it>
2021-05-24 17:43:13 +0100

    Try without git

--- dp-203/environment-3.ps1
+++ dp-203/environment-3.ps1
@@ -6,19 +6,15 @@ $azResourceGroupName = $env:AzResourceGroupName;
 Install-Module -Force SqlServer
 Import-Module -Force SqlServer
 
-apt-get update
-apt-get -y install git
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 if (Test-Path "./asa") {
     Get-Item "./asa" | Remove-Item -Force -Recurse
 }
 
-git config --global core.ignorecase true
-git clone https://github.com/godeploy/microsoft-data-engineering-ilt-deploy.git asa
-cd asa
-git checkout godeploy
-cd setup/04/artifacts/environment-setup/automation
+bash -c 'curl -L https://github.com/godeploy/microsoft-data-engineering-ilt-deploy/archive/godeploy.tar.gz | tar xzf -'
+mv ./microsoft-data-engineering-ilt-deploy-godeploy/ asa
+cd asa/setup/04/artifacts/environment-setup/automation
 
 az login -u $azUsername -p $azPassword
 
