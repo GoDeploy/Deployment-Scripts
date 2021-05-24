0aefd6e8542aeeb68fe08bbeec16c73d512accd4
root <root@GD-UK-LT-03.godeploy.labs>
2021-05-24 11:11:55 +0100

    Add environment-3.ps1

---
+++ dp-203/environment-3.ps1
@@ -0,0 +1,31 @@
+$azAdUserId = $env:AzAdUserId;
+$azUsername = $env:AzUsername;
+$azPassword = $env:AzPassword;
+$azResourceGroupName = $env:AzResourceGroupName;
+
+Install-Module -Force SqlServer
+Import-Module -Force SqlServer
+
+apt-get update
+apt-get -y install git
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+if (Test-Path "./asa") {
+    Get-Item "./asa" | Remove-Item -Force -Recurse
+}
+
+git config --global core.ignorecase true
+git clone https://github.com/godeploy/microsoft-data-engineering-ilt-deploy.git asa
+cd asa
+git checkout godeploy
+cd setup/04/artifacts/environment-setup/automation
+
+az login -u $azUsername -p $azPassword
+
+$azSecurePassword = $azPassword | ConvertTo-SecureString -AsPlainText -Force
+$azCredential = new-object -typename System.Management.Automation.PSCredential -argumentlist $azUsername, $azSecurePassword
+Connect-AzAccount -Credential $azCredential
+
+Write-Host "--- Setting up environment in $azResourceGroupName"
+./01-environment-setup.ps1 -resourceGroupName $azResourceGroupName
+
